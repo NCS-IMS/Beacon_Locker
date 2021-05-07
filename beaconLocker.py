baadde4056425c38859fcd7bde6a328fb8e8f8aa
Deuk-Hoi <dh97k@naver.com>
2021-05-07 21:32:35 +0100

    Connect GPIO and Open Process Success

--- beaconLocker.py
+++ beaconLocker.py
@@ -1,6 +1,8 @@
 # test BLE Scanning software
 # jcs 6/8/2014
 import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+import time
 import blescan
 import sys
 import bluetooth._bluetooth as bluez
@@ -18,6 +20,11 @@ except:
 blescan.hci_le_set_scan_parameters(sock)
 blescan.hci_enable_le_scan(sock)
 
+relayPin = 16
+openFlag = False
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(relayPin, GPIO.OUT)
+
 def on_connect(client, userdata, flag, rc):
     if rc == 0:
         print("connect OK")
@@ -32,6 +39,7 @@ def on_subscribe(client, userdata, mid, granted_qos):
 
 def on_message(client, userdata, msg):
 	global thread
+	global openFlag
 	print(msg.topic)
 	if msg.topic == "uuid":
 		userUUID = str(msg.payload.decode("utf-8"))
@@ -39,15 +47,28 @@ def on_message(client, userdata, msg):
 		thread.start()
 		
 	elif msg.topic =="test":
-		thread.do_run = False
+		if openFlag == True:
+			thread.do_run = False
+			GPIO.output(relayPin, GPIO.HIGH)
+			time.sleep(0.5)
+			GPIO.output(relayPin, GPIO.LOW)
+			openFlag = False
+			
 
 def startBeaconScanner(userUUID):
+	global openFlag
+	global thread
 	while getattr(thread, "do_run", True):
 		returnedList = blescan.parse_events(sock, 10)
 		print("-----")
 		for beacon in returnedList:
 			s = beacon.split(",")
-			if userUUID == s[1]:
+			if userUUID == s[1] and int(s[5]) >= -60 and openFlag == False:
+				GPIO.output(relayPin, GPIO.HIGH)
+				time.sleep(0.5)
+				GPIO.output(relayPin, GPIO.LOW)
+				openFlag = True
+				thread.do_run = False
 				print(s[1])
 
 
